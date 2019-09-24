9f92d650224340919f37666b0543e173eb643d1b
Bystroushaak <bystrousak@kitakitsune.org>
2019-09-25 00:11:31 +0200

    Rewritten to class based approach.

--- make.py
+++ make.py
@@ -4,50 +4,53 @@ import argparse
 from ebooklib import epub
 
 
-def generate_ebook(path):
-    book = epub.EpubBook()
+class BookGenerator:
+    def __init__(self):
+        self.book = epub.EpubBook()
+        self.title = 'Environment and the programming language Self'
+        self.chapters = []
 
-    # book.set_identifier('id123456')
-    title = 'Environment and the programming language Self'
-    book.set_title(title)
-    book.set_language('en')
-    book.add_author('Bystroushaak')
+        # book.set_identifier('id123456')
+        self.book.set_title(self.title)
+        self.book.set_language('en')
+        self.book.add_author('Bystroushaak')
 
-    # create chapter
-    c1 = epub.EpubHtml(title='Intro', file_name='chap_01.xhtml')
-    c1.content=u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
+    def generate_ebook(self, path):
+        self._add_first_chapter()
+        self._add_css()
+        self._add_toc()
 
-    # add chapter
-    book.add_item(c1)
+        epub.write_epub(path, self.book, {})
 
-    # define Table Of Contents
-    book.toc = (
-        epub.Link('chap_01.xhtml', 'Introduction', 'intro'),
-        (epub.Section(title),
-         (c1,))
-    )
+    def _add_first_chapter(self):
+        c1 = epub.EpubHtml(title='Intro', file_name='chap_01.xhtml')
+        c1.content=u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
 
-    # add default NCX and Nav file
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
+        self.book.add_item(c1)
+        self.chapters.append(c1)
 
-    # define CSS style
-    style = 'BODY {color: white;}'
-    nav_css = epub.EpubItem(
-        uid="style_nav",
-        file_name="style/nav.css",
-        media_type="text/css",
-        content=style
-    )
+    def _add_toc(self):
+        self.book.toc = (
+            (epub.Section(self.title),
+             self.chapters),
+        )
 
-    # add CSS file
-    book.add_item(nav_css)
+        self.book.add_item(epub.EpubNcx())
+        self.book.add_item(epub.EpubNav())
 
-    # basic spine
-    book.spine = ['nav', c1]
+        self.book.spine = ['nav'] + self.chapters
 
-    # write to the file
-    epub.write_epub(path, book, {})
+    def _add_css(self):
+        # define CSS style
+        style = 'BODY {color: white;}'
+        nav_css = epub.EpubItem(
+            uid="style_nav",
+            file_name="style/nav.css",
+            media_type="text/css",
+            content=style
+        )
+
+        self.book.add_item(nav_css)
 
 
 if __name__ == '__main__':
@@ -59,4 +62,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    generate_ebook('test.epub')
+    book = BookGenerator()
+    book.generate_ebook('test.epub')
