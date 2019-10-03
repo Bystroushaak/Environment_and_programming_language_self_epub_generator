1467fb202e8b6bae937d97ed6afa1dfd44a658f5
Bystroushaak <bystrousak@kitakitsune.org>
2019-10-04 01:02:19 +0200

    Conversion script now converts all chapters + images. Added foreword.

--- make.py
+++ make.py
@@ -27,6 +27,9 @@ class BookGenerator:
         self.book.add_item(chapter)
         self.chapters.append(chapter)
 
+    def add_image(self, image):
+        self.book.add_item(image)
+
     def _add_toc(self):
         self.book.toc = (
             (epub.Section(self.title),
@@ -51,66 +54,102 @@ class BookGenerator:
         self.book.add_item(nav_css)
 
 
-def pick_first_chapter(html_path):
-    article_path = 'Environment and the programming language Self part.html'
+class EAPLSEpub:
+    def __init__(self, html_root):
+        self.html_root = html_root
+        self.book = BookGenerator()
 
-    with open(os.path.join(html_path, article_path)) as f:
-        data = f.read()
-        dom = dhtmlparser.parseString(data)
+        self.add_foreword()
 
-    title = dom.find("title")[0].getContent()
-    title = title.split("(")[-1].split(")")[0].capitalize()
+        self.chapters_metdata = [
+            ('Environment and the programming language Self part.html',
+             'chap_01.xhtml'),
+            ('Environment and the programming language Self part 1.html',
+             'chap_02.xhtml'),
+            ('Environment and the programming language Self part 2.html',
+             'chap_03.xhtml'),
+            ('Environment and the programming language Self part 3.html',
+             'chap_04.xhtml'),
+        ]
 
-    c1 = epub.EpubHtml(title=title, file_name='chap_01.xhtml')
+        for article_path, chapter_fn in self.chapters_metdata:
+            self.convert_chapter(article_path, chapter_fn)
 
-    body = dom.find("div", {"class":"page-body"})[0]
+    def add_foreword(self):
+        chapter = epub.EpubHtml(title="Foreword", file_name="foreword.html")
+        chapter.content = """
+<p>This book was converted from the HTML articles originally published at my blog
+<a href="http://blog.rfox.eu">blog.rfox.eu</a>.</p>
 
-    while body.childs[0].getTagName() != "hr":
+<p>I want to apologize for the low quality of the export. If you feel like you
+want to improve the quality, your contribution is most welcomed:<p>
+
+<ul>
+    <li><a href=""></a></li>
+</ul> 
+        """
+        self.book.add_chapter(chapter)
+
+    def convert_chapter(self, article_path, chapter_fn, title=None):
+        with open(os.path.join(self.html_root, article_path)) as f:
+            dom = dhtmlparser.parseString(f.read())
+
+        if not title:
+            title = dom.find("title")[0].getContent()
+            title = title.split("(")[-1].split(")")[0].capitalize()
+
+        body = dom.find("div", {"class":"page-body"})[0]
+
+        self._remove_fluff_from_the_beginning(body)
+        self._remove_fluff_from_the_end(body)
+        self._inline_images(body)
+
+        chapter = epub.EpubHtml(title=title, file_name=chapter_fn)
+        chapter.content = body.getContent()
+        self.book.add_chapter(chapter)
+
+    def _remove_fluff_from_the_beginning(self, body):
+        while body.childs[0].getTagName() != "hr":
+            body.childs.pop(0)
         body.childs.pop(0)
-    body.childs.pop(0)
 
-    while body.childs[-1].getContent() != "Next episodes":
+    def _remove_fluff_from_the_end(sefl, body):
+        look_for = ["Next episodes", "Next episode", "Last episode", "Relevant discussions"]
+
+        selected_phrase = ""
+        for phrase in look_for:
+            if body.find("h1", fn=lambda x: x.getContent() == phrase):
+                selected_phrase = phrase
+                break
+        else:
+            return
+
+        while body.childs[-1].getContent() != selected_phrase:
+            body.childs.pop()
         body.childs.pop()
-    body.childs.pop()
 
-    c1.content = body.getContent()
+    def _inline_images(self, body):
+        for img in body.find("img"):
+            epub_img = epub.EpubImage()
 
-    return c1
+            epub_img.file_name = os.path.basename(img.params["src"])
+            image_path = os.path.join(self.html_root, img.params["src"])
+            with open(image_path, "rb") as f:
+                epub_img.content = f.read()
 
+            if "style" in img.params:
+                del img.params["style"]
 
-def pick_second_chapter(html_path):
-    article_path = 'Environment and the programming language Self part 1.html'
-    c2 = epub.EpubHtml(title='Second', file_name='chap_02.xhtml')
-    c2.content = u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
+            self.book.add_image(epub_img)
+            img.params["src"] = epub_img.file_name
 
-    return c2
-
-
-def pick_third_chapter(html_path):
-    article_path = 'Environment and the programming language Self part 2.html'
-    c3 = epub.EpubHtml(title='Third', file_name='chap_03.xhtml')
-    c3.content = u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
-
-    return c3
-
-
-def pick_fourth_chapter(html_path):
-    article_path = 'Environment and the programming language Self part 3.html'
-    c4 = epub.EpubHtml(title='Fourth', file_name='chap_04.xhtml')
-    c4.content = u'<h1>Intro heading</h1><p>Zaba je skocila u baru.</p>'
-
-    return c4
+    def generate_ebook(self, path):
+        return self.book.generate_ebook(path)
 
 
 def put_ebook_together(html_path):
-    book = BookGenerator()
-
-    book.add_chapter(pick_first_chapter(html_path))
-    # book.add_chapter(pick_second_chapter(html_path))
-    # book.add_chapter(pick_third_chapter(html_path))
-    # book.add_chapter(pick_fourth_chapter(html_path))
-
-    book.generate_ebook('test.epub')
+    book = EAPLSEpub(html_path)
+    book.generate_ebook('environment_and_programming_language_self_2019.epub')
 
 
 if __name__ == '__main__':
